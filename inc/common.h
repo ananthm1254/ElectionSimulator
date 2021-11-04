b7d8de29cfa99ead0fe86706c01a191597c8c16b
ananthm1254 <ananth7121999@gmail.com>
2021-11-04 13:36:59 +0530

    Placeholder code

---
+++ inc/common.h
@@ -0,0 +1,4 @@
+#pragma once
+
+typedef long long int int64_t;
+typedef int int32_t;
---
+++ inc/voter.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "common.h"
+
+typedef struct Voter{
+    int64_t tag;        // identifier tag
+    int32_t age;        //age of voter (important factor)
+    double issue_1;     //placeholder
+}Voter_t;
